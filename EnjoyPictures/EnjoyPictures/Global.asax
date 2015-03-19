0e88f9b82a31e32af09d032ae27bc8e68a82b43f
YingXia <191502003@qq.com>
2015-03-19 20:45:04 +0800

    new project

---
+++ EnjoyPictures/EnjoyPictures/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnjoyPictures.MvcApplication" Language="C#" %>
---
+++ EnjoyPictures/EnjoyPictures/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnjoyPictures.Startup))]
+namespace EnjoyPictures
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
